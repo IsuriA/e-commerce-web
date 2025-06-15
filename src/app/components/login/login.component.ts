380f45cb82bfe4f445279805147ad72e3ccdbd76
Vajira Sanjaya <vsanjaya163@gmail.com>
2025-06-15 22:43:22 +0800

    display alert on login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,17 +3,19 @@ import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   loginForm: FormGroup;
   authService = inject(AuthService)
   router = inject(Router);
+  snackBar = inject(MatSnackBar);
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
@@ -26,11 +28,28 @@ export class LoginComponent {
 
     if (this.loginForm.valid) {
       this.authService.login(this.loginForm.value)
-        .subscribe((result: any) => {
-          if (this.authService.isLoggedIn()) {
-            this.router.navigate(['home']);
+        .subscribe(
+          data => {
+            this.snackBar.open('Login successfull', 'Close', {
+              duration: 3000, // Optional duration in milliseconds
+              horizontalPosition: 'end',
+              verticalPosition: 'top',
+              panelClass: 'notification-success',
+            });
+
+            if (this.authService.isLoggedIn()) {
+              this.router.navigate(['home']);
+            }
+          },
+          err => {
+            this.snackBar.open(err.error.message, 'Close', {
+              duration: 3000, // Optional duration in milliseconds
+              horizontalPosition: 'end',
+              verticalPosition: 'top',
+              panelClass: 'notification-error',
+            });
           }
-        });
+        );
     }
   }
 }
